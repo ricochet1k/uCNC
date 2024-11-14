5246b07c241231a5bba0d3bc170f014c8488141e
Matt Peterson <ricochet1k@gmail.com>
2024-11-13 17:06:08 -0700

    pack all the bitfields

--- uCNC/src/modules/tmc_driver/tmc_bitfields.h
+++ uCNC/src/modules/tmc_driver/tmc_bitfields.h
@@ -39,7 +39,7 @@ extern "C"
 		union
 		{
 			uint32_t sr;
-			struct
+			struct  __attribute__((packed))
 			{
 				uint8_t i_scale_analog : 1;
 				uint8_t internal_rsense : 1;
@@ -53,7 +53,7 @@ extern "C"
 				uint8_t test_mode : 1;
 			};
 
-			struct
+			struct  __attribute__((packed))
 			{
 				uint8_t : 2; // 2130; 5130
 				uint8_t en_pwm_mode : 1;
@@ -72,7 +72,7 @@ extern "C"
 				uint8_t stop_enable : 1;
 				uint8_t direct_mode : 1;
 			} tmc2130;
-			struct
+			struct  __attribute__((packed))
 			{ // TMC5160
 				uint8_t recalibrate : 1;
 				uint8_t faststandstill : 1;
@@ -92,7 +92,7 @@ extern "C"
 		union
 		{
 			uint32_t sr : 20;
-			struct
+			struct  __attribute__((packed))
 			{
 				uint8_t ihold : 5;
 				uint8_t : 3;
@@ -108,7 +108,7 @@ extern "C"
 		union
 		{
 			uint32_t sr;
-			struct
+			struct  __attribute__((packed))
 			{
 				uint8_t toff : 4;
 				uint8_t hstrt : 3;
@@ -124,9 +124,9 @@ extern "C"
 				uint8_t diss2vs : 1;
 			};
 
-			struct
+			struct  __attribute__((packed))
 			{
-				uint16_t : 12;
+				uint16_t : 11;
 				uint8_t fd3 : 1;
 				uint8_t disfdcc : 1;
 				uint8_t rndtf : 1;
@@ -138,7 +138,7 @@ extern "C"
 				uint8_t : 8;
 			} tmc2130;
 
-			struct
+			struct  __attribute__((packed))
 			{ // TMC5160
 				uint32_t : 20;
 				uint8_t tpfd : 4; // 5160
@@ -152,7 +152,7 @@ extern "C"
 		union
 		{
 			uint32_t sr;
-			struct
+			struct  __attribute__((packed))
 			{
 				uint8_t pwm_ofs : 8;
 				uint8_t pwm_grad : 8;
@@ -164,7 +164,7 @@ extern "C"
 				uint8_t pwm_reg : 4;
 				uint8_t pwm_lim : 4;
 			};
-			struct
+			struct  __attribute__((packed))
 			{
 				uint8_t pwm_ampl : 8;
 				uint16_t : 11;
@@ -173,7 +173,7 @@ extern "C"
 		};
 	} PWMCONF_t;
 
-	typedef struct SGTHRS_
+	typedef struct  __attribute__((packed)) SGTHRS_
 	{
 		uint8_t sr : 8;
 	} SGTHRS_t;
@@ -183,7 +183,7 @@ extern "C"
 		union
 		{
 			uint32_t sr;
-			struct
+			struct  __attribute__((packed))
 			{
 				uint8_t semin : 4;
 				uint8_t : 1;
@@ -194,7 +194,7 @@ extern "C"
 				uint8_t sedn : 2;
 				uint8_t seimin : 1;
 			};
-			struct
+			struct  __attribute__((packed))
 			{
 				uint16_t : 16;
 				int8_t sgt : 7;
@@ -209,7 +209,7 @@ extern "C"
 		union
 		{
 			uint32_t sr;
-			struct
+			struct  __attribute__((packed))
 			{
 				uint8_t otpw : 1;
 				uint8_t ot : 1;
@@ -230,7 +230,7 @@ extern "C"
 				uint8_t stealth : 1;
 				uint8_t stst : 1;
 			};
-			struct
+			struct  __attribute__((packed))
 			{
 				uint16_t sg_result : 10;
 				uint8_t : 5;
